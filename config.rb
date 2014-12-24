f73c8735b0252339af8ccc0ee65bb7865d6cd18e
chenchunsheng <chenchunsheng@smartisan.cn>
2014-12-24 15:13:58 +0800

    Initial source commit

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/webrtcblog/"
+http_images_path = "/webrtcblog/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/webrtcblog/fonts"
+css_dir = "public/webrtcblog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/_posts/2014-12-24-first-test-for-octopress.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title: "first test for octopress"
+date: 2014-12-24 14:40:20 +0800
+comments: true
+categories: 
+---
